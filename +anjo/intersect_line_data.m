8c4c22a69e0d3c4b8deebcdc4c12f03ad9620d39
Andreas Johlander <andreas@Andreass-MacBook-Pro.local>
2015-02-20 11:24:48 +0100

    fixes intersect_line_data

--- +anjo/intersect_line_data.m
+++ +anjo/intersect_line_data.m
@@ -14,23 +14,19 @@ interInd = find(yFlat(1:end-1).*yFlat(2:end)<=0,1);
 %xIntersect = lX(interInd);
 
 
-interInd 
-len
 %refined
 if(interInd == len)
     kA = diff(yData(interInd-1:interInd))/diff(xData(interInd-1:interInd));
     mA = yData(interInd)-kA*xData(interInd);
-    xIntersect = (m-mA)/(kA-k);
 elseif(interInd == 1)
-    
     kA = diff(yData(interInd:interInd+1))/diff(xData(interInd:interInd+1));
     mA = yData(interInd)-kA*xData(interInd);
-    xIntersect = (m-mA)/(kA-k);
 else
-    
     kA = diff(yData(interInd:interInd+1))/diff(xData(interInd:interInd+1));
     mA = yData(interInd)-kA*xData(interInd);
-    xIntersect = (m-mA)/(kA-k);
-    
 end
+
+
+xIntersect = (m-mA)/(kA-k);
+yIntersect = k*xIntersect+m;
 end
