585d7331180b8230269866845b307e7537004b3e
Andreas Johlander <andreas@Andreass-MacBook-Pro.local>
2015-02-23 11:45:56 +0100

    changes structure to plot_3d_b_field and adds get_3d_b_field

--- +anjo/plot_3d_b_field.m
+++ +anjo/plot_3d_b_field.m
@@ -1,55 +1,52 @@
 function [bField] = plot_3d_b_field(h,tint,scInd,plotMode)
-%PLOT_3D_B_FIELD plot 3D FGM data in GSE.
-%   PLOT_3D_E_FIELD(h,tint,scInd)
+%ANJO.PLOT_3D_B_FIELD plot 3D FGM data in GSE.
+%   ANJO.PLOT_3D_B_FIELD(h,tint,scInd) - plots components and amplitude of
+%   magnetic field for time interval tint for spacecraft scInd.
+%   ANJO.PLOT_3D_B_FIELD(h,tint,scInd,plotMode)
+%   mode:
+%       'default'   - plots components and amplitude.
+%       '3d'        - plots only components.
+%       'abs'       - plots only amplitude.
+%
+%   bField = ANJO.PLOT_3D_B_FIELD(h,tint,scInd) also returns magnetic field.
+%
+%   See also: ANJO.GET_3D_B_FIELD, ANJO.GET_3D_E_FIELD,
+%   ANJO.PLOT_3D_E_FIELD
 
-hideXLabel = isequal(get(h,'XTickLabel'),[]);
+
+%hideXLabel = isequal(get(h,'XTickLabel'),[]);
 
 if(nargin < 4)
     plotMode = 'default';
 end
 
-if(scInd == 1 || scInd == 2 || scInd == 3 || scInd == 4)
-    dataStr = ['B_vec_xyz_gse__C',num2str(scInd),'_CP_FGM_FULL'];
-    bField = local.c_read(dataStr,tint);
-    
-else
-    bField = zeros(1,4);
-    disp('Not a spacecraft!')
-    return;
-end
 
+bField = get_3d_b_field(tint,scInd,plotMode);
 
 switch plotMode
     case 'default'
-        normB = sqrt(sum(bField(:,2:4).^2,2));
-        bField = [bField,normB];
         legStr = {'B_{x}','B_{y}','B_{z}','|B|'};
     case '3d'
         legStr = {'B_{x}','B_{y}','B_{z}'};
-        
+
     case 'abs'
-        normB = sqrt(sum(bField(:,2:4).^2,2));
-        bField = [bField(:,1),normB];
         scColor = anjo.get_cluster_colors();
-        
+
         set(h,'ColorOrder',[scColor{scInd}])
         %No legend
 end
 
-
-hL = irf_plot(h,bField);
+irf_plot(h,bField);
 
 
 if(strcmp(plotMode,'abs'))
     
 else
     legLD = [0.02,0.06];
-    hLeg = irf_legend(h,legStr,legLD);
-    %ylabel(h,'B [nT]','FontSize',20);
+    irf_legend(h,legStr,legLD);
 end
+
 ylabel(h,'$B$ [nT]','FontSize',16,'interpreter','latex');
-
-
 
 irf_zoom(h,'x',tint)
 
