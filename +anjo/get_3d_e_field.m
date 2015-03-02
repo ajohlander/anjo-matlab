a05e2e4b5c7b852fb1ca088a27af48637d78d4b8
Andreas Johlander <andreas@nl119-144-237.student.uu.se>
2015-03-02 11:59:15 +0100

    adds transform_e_field and fixes help text of get_3d_e_field

--- +anjo/get_3d_e_field.m
+++ +anjo/get_3d_e_field.m
@@ -1,6 +1,10 @@
 function [eField] = get_3d_e_field(tint,scInd)
-%ANJO.GET_3D_E_FIELD Summary of this function goes here
-%   AAAAAAAAAAAAAAAAAAAA TYPING!!!!!!!!
+%ANJO.GET_3D_E_FIELD returns 3D EFW data in ISR2.
+%   bField = ANJO.GET_3D_E_FIELD(tint,scInd) - returns 3D electic field
+%   for time interval tint and for spacecraft scInd.
+
+%   See also: ANJO.PLOT_3D_E_FIELD, ANJO.GET_3D_E_FIELD,
+%   ANJO.PLOT_3D_B_FIELD
 
 if(scInd == 1 || scInd == 2 || scInd == 3 || scInd == 4)
     dataStr = ['E_Vec_xyz_ISR2__C',num2str(scInd),'_CP_EFW_L2_E3D_INERT'];
