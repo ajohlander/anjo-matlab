46ced6cbfcbceb9cdfaef3d1295231e0a82ee57b
Andreas Johlander <andreas@nl119-154-138.student.uu.se>
2015-03-11 13:39:48 +0100

    enables only time input

--- +anjo/fast_date.m
+++ +anjo/fast_date.m
@@ -2,6 +2,11 @@ function [tOut] = fast_date(tIn,showMilliSeconds)
 %ANJO.FAST_DATE Returns a date string
 %   tOut = ANJO.FAST_DATE(tIn, showMilliSeconds) returns the date of tIn,
 %   which is in Unix time. showMilliSeconds is a boolean.
+%   tOut = ANJO.FAST_DATE(tIn) shows milliseconds by default.
+
+if(nargin == 1)
+    showMilliSeconds = 1; %Default
+end
 
 if (showMilliSeconds ~= 1 && showMilliSeconds ~= 0)
     showMilliSeconds = 1;
