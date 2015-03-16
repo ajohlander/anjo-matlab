aeb6b84b8c0369ca8877a3c61cf3949a2412ab6d
ajohlander <andreas.johlander@gmail.com>
2015-03-16 17:01:30 +0100

    update to get_cluster_colors

--- +anjo/get_cluster_colors.m
+++ +anjo/get_cluster_colors.m
@@ -1,10 +1,16 @@
-function [c] = get_cluster_colors()
+function [c] = get_cluster_colors(scInd)
 %ANJO.GET_CLUSTER_COLORS get line colors for the Cluster satellites
 %   c = ANJO.GET_CLUSTER_COLORS() returns colors c = 1x4 cell.
+%   c = ANJO.GET_CLUSTER_COLORS(scInd) returns color for spacecraft scInd.
+%   
 %   Mostly to get the same colors in all figures.
 
-c = {[0,0,0],[1,0,0],[0,0.8,0],[0,0,1]};
-
+clCol = {[0,0,0],[1,0,0],[0,0.8,0],[0,0,1]};
+if(nargin == 0)
+    c = clCol;
+else
+    c = clCol{scInd};
+end
 
 end
 
