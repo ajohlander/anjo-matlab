efb47cee20c288a95089f933d47fe72864244428
ajohlander <andreas.johlander@gmail.com>
2015-03-18 15:56:15 +0100

    Adds Contents.m for documentation and help

---
+++ +anjo/Contents.m
@@ -0,0 +1,37 @@
+% +ANJO
+% Version 0 18-mar-2015
+%
+% Files
+%   afigure             - ANJO.AFIGURE Quick way to call irf_plot
+%   align_y_labels      - ANJO.ALIGN_Y_LABELS Fixes the x-position of the y-labels
+%   car2sph             - ANJO.CAR2SPH Transforms coordinates from cartesian to spherical
+%   compress_field      - ANJO.COMPRESS_FIELD compresses 3D E- or B-field 
+%   e_jxb               - ANJO.E_JXB Calculates E = jxB/ne. 
+%   e_vxb               - ANJO.E_VXB Calculates E = jxB/ne. 
+%   fast_date           - ANJO.FAST_DATE Returns a date string
+%   find_closest_index  - ANJO.FIND_CLOSEST_INDEX Finds relevant index of a vector
+%   get_3d_b_field      - ANJO.GET_3D_B_FIELD returns 3D FGM data in GSE.
+%   get_3d_e_field      - ANJO.GET_3D_E_FIELD returns 3D EFW data in ISR2.
+%   get_c_pos           - ANJO.GET_C_POS Returns the position of all Cluster satellites
+%   get_cluster_colors  - ANJO.GET_CLUSTER_COLORS get line colors for the Cluster satellites
+%   get_comb_spin       - ANJO.GET_COMB_SPIN Combines ion data from two spins
+%   get_hia_data        - ANJO.GET_HIA_DATA returns data from HIA in phase space density [s^3km^-6].
+%   get_hia_values      - ANJO.GET_HIA_VALUES Returns instrument parameters for HIA
+%   get_one_hia_spin    - ANJO.GET_ONE_HIA_SPIN get ion data for one full spin of CIS-HIA.
+%   get_part_spin       - ANJO.GET_PART_SPIN Get a partial spin
+%   gse2nmr             - Awesome function
+%   gse2snr             - Awesome function
+%   import_c_data       - ANJO.IMPORT_C_DATA Downloads data from CSA, stores it in ~/Data/caalocal.
+%   intersect_line_data - ANJO.INTERSECT_LINE_DATA Finds intersection between a line and data points
+%   is_new_matlab       - ANJO.IS_NEW_MATLAB Checks if new plot system is to be used
+%   lorentz_1d          - ANJO.LORENTZ_1D performs a 1D test patrticle simulation
+%   plot_3d_b_field     - ANJO.PLOT_3D_B_FIELD plot 3D FGM data in GSE.
+%   plot_3d_e_field     - ANJO.PLOT_3D_E_FIELD plot 3D EFW data in ISR2.
+%   plot_hia_subspin    - Summary of this function goes here
+%   plot_ion_polar_isr2 - plot CIS-HIA data in velocity space.
+%   plot_sim_vel        - ANJO.PLOT_SIM_VEL plots final velocity vs initial velocity
+%   print_fig           - ANJO.PRINT_FIG Exports figure to file.
+%   progress_pie        - pie chart showing progress of a calculation
+%   reflect_vector      - 
+%   sph2car             - Transforms coordinates from spherical to cartesian
+%   transform_e_field   - ANJO.TRANSFORM_E_FIELD Transforms electric field to another inertial frame
