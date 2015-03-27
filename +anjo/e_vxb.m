9eace0eeac0a501fa28ddcb04aa31509f2cef68d
ajohlander <andreas.johlander@gmail.com>
2015-03-27 14:42:28 +0100

    updates e_vxb

--- +anjo/e_vxb.m
+++ +anjo/e_vxb.m
@@ -1,9 +1,10 @@
-function [eF] = e_vxb(bF,v,alpha)
-%ANJO.E_VXB Calculates E = jxB/ne. 
-%   eF = ANJO.E_VXB(bF,v) returns electric field given magnetic field bF
-%   and plasma bulk velocity v and number density n. Electric field is
-%   calculated by: E = -v x B.
-%   eF = ANJO.E_VXB(bF,v,alpha) introcuces a scalar constant alpha so
+function [eF] = e_vxb(v,B,alpha)
+%ANJO.E_VXB Calculates E = -vxB. 
+%   E = ANJO.E_VXB(v,B) returns electric field given magnetic field bF [nT]
+%   and plasma bulk velocity v [km/s]. Electric field is calculated by:
+%   E = -v x B and is returned in [mV/m].
+%   
+%   E = ANJO.E_VXB(v,B,alpha) introduces a scalar constant alpha so
 %   that: E = alpha*(-v x B).
 %
 %   See also: ANJO.E_JXB, ANJO.TRANSFORM_E_FIELD
@@ -14,15 +15,13 @@ if(nargin == 2)
     alpha = 1;
 end
 
-eF = bF; % same size and times
+vel = irf_resamp(v,B); % resample velocity 
 
-for i = 1:size(bF,1)
-    B = bF(i,2:4)/1e9; % B-vector in T
-        % interpolates velocity for the time point [m/s]
-    vel = interp1(v(:,1),v(:,2:4),bF(i,1))*1e3;
-    % Calculates electric field E=-vxB
-    eF(i,2:4) = -alpha*cross(vel,B)*1e3;
-end
+eF = irf_cross(vel,B); % Calculates E = vxB 
+
+eF(:,2:4) = -alpha*eF(:,2:4)*1e3*1e3*1e-9; % Factor -alpha*1e-3 [mV/m]
+
+
 
 end
 
