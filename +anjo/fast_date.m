435edf17beab943395cb920f63d9ac3cc436b29d
ajohlander <andreas.johlander@gmail.com>
2015-04-01 13:10:22 +0200

    rounds seconds and changes order of modes

--- +anjo/fast_date.m
+++ +anjo/fast_date.m
@@ -9,8 +9,8 @@ function [tOut] = fast_date(tIn,mode)
 %   Mode:
 %       0   -   yyyy-mm-dd HH:MM:SS
 %       1   -   yyyy-mm-dd HH:MM:SS.FFF
-%       2   -   HH:MM:SS.FFF
-%       3   -   HH:MM:SS
+%       2   -   HH:MM:SS
+%       3   -   HH:MM:SS.FFF
 %
 %   Example
 %       tint = [irf_time([2002 02 03 04 18 00]), irf_time([2002 02 04 04 18 23])];
@@ -20,6 +20,12 @@ function [tOut] = fast_date(tIn,mode)
 if(nargin == 1)
     mode = 1; %Default
 end
+
+% If resolution is seconds, then round
+if(mode/2 == floor(mode/2)) % even
+    tIn = round(tIn);
+end
+
 
 % Start of Unix time
 t0 = datenum([1970 01 01 00 00 00]);
@@ -31,16 +37,18 @@ switch mode
     case 0
         tOut = datestr(tVal,...
             'yyyy-mm-dd HH:MM:SS');
+        
     case 1
         tOut = datestr(tVal,...
             'yyyy-mm-dd HH:MM:SS.FFF');
-
-    case 2  %short exact
-        tOut = datestr(tVal,...
-            'HH:MM:SS.FFF');
-    case 3  %short 
+        
+    case 2  %short
         tOut = datestr(tVal,...
             'HH:MM:SS');
+        
+    case 3  %short exaxt
+        tOut = datestr(tVal,...
+            'HH:MM:SS.FFF');
 end
 
 end
