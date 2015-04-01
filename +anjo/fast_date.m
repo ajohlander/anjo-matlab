acb54179f277ce9203739891942f319f65f4eb95
ajohlander <andreas.johlander@gmail.com>
2015-04-01 12:05:35 +0200

    adds options for shorter format

--- +anjo/fast_date.m
+++ +anjo/fast_date.m
@@ -1,21 +1,46 @@
-function [tOut] = fast_date(tIn,showMilliSeconds)
+function [tOut] = fast_date(tIn,mode)
 %ANJO.FAST_DATE Returns a date string
-%   tOut = ANJO.FAST_DATE(tIn, showMilliSeconds) returns the date of tIn,
-%   which is in Unix time. showMilliSeconds is a boolean.
-%   tOut = ANJO.FAST_DATE(tIn) shows milliseconds by default.
+%   tOut = ANJO.FAST_DATE(tIn) returns a date string of tIn,
+%   which is in Unix time.
+%   
+%   tOut = ANJO.FAST_DATE(tIn,mode) specify the type of string reurned.
+%   Default is mode=1.
+%
+%   Mode:
+%       0   -   yyyy-mm-dd HH:MM:SS
+%       1   -   yyyy-mm-dd HH:MM:SS.FFF
+%       2   -   HH:MM:SS.FFF
+%       3   -   HH:MM:SS
+%
+%   Example
+%       tint = [irf_time([2002 02 03 04 18 00]), irf_time([2002 02 04 04 18 23])];
+%       tOut = anjo.fast_date(tint,1);
+
 
 if(nargin == 1)
-    showMilliSeconds = 1; %Default
+    mode = 1; %Default
 end
 
-if (showMilliSeconds ~= 1 && showMilliSeconds ~= 0)
-    showMilliSeconds = 1;
-end
+% Start of Unix time
+t0 = datenum([1970 01 01 00 00 00]);
 
-if showMilliSeconds
-    tOut = datestr(tIn/(3600*24) + datenum([1970 01 01 00 00 00]),'yyyy-mm-dd HH:MM:SS.FFF');
-else
-    tOut = datestr(tIn/(3600*24) + datenum([1970 01 01 00 00 00]),'yyyy-mm-dd HH:MM:SS');
+% Value of tIn in some other time 
+tVal = tIn/(3600*24)+t0;
+
+switch mode
+    case 0
+        tOut = datestr(tVal,...
+            'yyyy-mm-dd HH:MM:SS');
+    case 1
+        tOut = datestr(tVal,...
+            'yyyy-mm-dd HH:MM:SS.FFF');
+
+    case 2  %short exact
+        tOut = datestr(tVal,...
+            'HH:MM:SS.FFF');
+    case 3  %short 
+        tOut = datestr(tVal,...
+            'HH:MM:SS');
 end
 
 end
