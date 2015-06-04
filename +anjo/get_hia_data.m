ff332ad97831c7f3622e42824bc84d49be787ebc
ajohlander <andreas.johlander@gmail.com>
2015-06-04 11:43:45 +0200

    changes sum_over_pol to 3d input and cleans alot in the code

--- +anjo/get_hia_data.m
+++ +anjo/get_hia_data.m
@@ -1,4 +1,4 @@
-function [ionMat,t] = get_hia_data(tint,scInd,dataMode)
+function [F,t] = get_hia_data(tint,scInd,dataMode)
 %ANJO.GET_HIA_DATA returns data from HIA in phase space density [s^3km^-6].
 %   [ionMat,t] = ANJO.GET_HIA_DATA(tint,scInd,mode) returns data in
 %   matrix ionMat and time vector t. tint is the time interval for the
@@ -9,6 +9,7 @@ function [ionMat,t] = get_hia_data(tint,scInd,dataMode)
 %       '3d'        - like default but matrix N*16x8x31
 %       'energy'    - integrates over polar angle
 %       'polar'     - integrates over energy
+%       '1d'        - integrates over azimutal and polar angle
 %
 %   See also: ANJO.GET_ONE_HIA_SPIN
 %
@@ -18,83 +19,84 @@ if(scInd == 1 || scInd == 3)
     dataStr = ['3d_ions__C',num2str(scInd),'_CP_CIS-HIA_HS_MAG_IONS_PSD'];
 else
     disp('NO HIA DATA');
-    ionMat = zeros(8,16,31);
+    F = zeros(8,16,31);
     return;
 end
 
 ion3d = local.c_read(dataStr,tint);
-ionArray = double(ion3d{2});
-tArray = double(ion3d{1});
+F_4d = (ion3d{2});
+tArray = (ion3d{1});
 
 if(nargin == 3)
     if(nargin == 3 && strcmp(dataMode,'default'))
         % do nothing
-        ionMat = ionArray;
+        F = F_4d;
         t = tArray;
         
     elseif(strcmp(dataMode,'energy'))
         % sum over polar angle
-%         ionArray = squeeze(sum(ionArray,2));
-        ionArray = anjo.hia_sum_over_pol(ionArray);
-        tNum = size(ionArray,1);
-        ionMat = zeros(tNum*16,31);
-        t = zeros(1,tNum*16);
-        
-        for i = 1:tNum
-            tInd = (i-1)*16+1;
-            ionMat(tInd:tInd+15,:) = squeeze(ionArray(i,:,:));
-            
-            %recalculate tSpin everytime
-            if(i == tNum)
-                tSpin = mean(diff(tArray));
-            else
-                tSpin = tArray(i+1)-tArray(i);
-            end
-            t(tInd:tInd+15) = tArray(i)+linspace(0,tSpin*15/16,16);
-        end
+        [t,F_3d] = hia_sum_over_spin(tArray,F_4d);
+        F_2d = anjo.hia_sum_over_pol(F_3d);
+        F = F_2d;
         
     elseif(strcmp(dataMode,'polar'))
         % sum over energy
-        ionArray = squeeze(sum(ionArray,4));
-        tNum = size(ionArray,1);
-        ionMat = zeros(tNum*16,8);
-        t = zeros(1,tNum*16);
-        
-        for i = 1:tNum
-            tInd = (i-1)*16+1;
-            ionMat(tInd:tInd+15,:) = squeeze(ionArray(i,:,:))';
-            
-            %recalculate tSpin everytime
-            if(i == tNum)
-                tSpin = mean(diff(tArray));
-            else
-                tSpin = tArray(i+1)-tArray(i);
-            end
-            t(tInd:tInd+15) = tArray(i)+linspace(0,tSpin*15/16,16);
-        end
+        [t,F_3d] = hia_sum_over_spin(tArray,F_4d);
+        F_2d = hia_sum_over_energy(F_3d);
+        F = F_2d;
         
     elseif(strcmp(dataMode,'3d'))
-        
-        tNum = size(ionArray,1);
-        ionMat = zeros(8,tNum*16,31);
-        t = zeros(1,tNum*16);
-        
-        
-        for i = 1:tNum
-            tInd = (i-1)*16+1;
-            ionMat(:,tInd:tInd+15,:) = squeeze(ionArray(i,:,:,:));
-            
-            %recalculate tSpin everytime
-            if(i == tNum)
-                tSpin = mean(diff(tArray));
-            else
-                tSpin = tArray(i+1)-tArray(i);
-            end
-            t(tInd:tInd+15) = tArray(i)+linspace(0,tSpin*15/16,16);
-        end
-        ionMat = permute(ionMat,[2,1,3]);
+        [t,F] = hia_sum_over_spin(tArray,F_4d);
+    elseif(strcmp(dataMode,'1d'))
+        [t,F_3d] = hia_sum_over_spin(tArray,F_4d);
+        F_2d = anjo.hia_sum_over_pol(F_3d);
+        F_1d = hia_sum_over_az(F_2d);
+        F = F_1d;
     end
 end
 
 
+
+
+
 end
+
+
+function [t,F_3d] = hia_sum_over_spin(tArray,F_4d) 
+% SQ4D3D Transforms 4d ion matrix to 3d ion matrix
+
+N = size(F_4d,1);
+F_3d = zeros(8,N*16,31);
+t = zeros(1,N*16);
+
+for i = 1:N
+    tInd = (i-1)*16+1;
+    F_3d(:,tInd:tInd+15,:) = squeeze(F_4d(i,:,:,:));
+    
+    %recalculate tSpin everytime
+    if(i == N)
+        tSpin = mean(diff(tArray));
+    else
+        tSpin = tArray(i+1)-tArray(i);
+    end
+    t(tInd:tInd+15) = tArray(i)+linspace(0,tSpin*15/16,16);
+end
+
+end
+
+function [F_1d] = hia_sum_over_az(F_2d)
+% Input is 3d matrix, size:     16Nx31
+% Output is 2d matrix, size:    1x31
+
+F_1d = sum(F_2d,1);
+
+end
+
+function [F_2d] = hia_sum_over_energy(F_3d)
+% Input is 3d matrix, size:     8x16Nx31
+% Output is 2d matrix, size:    8*16N
+
+F_2d = squeeze(sum(F_3d,3));
+
+end
+
--- +anjo/hia_sum_over_pol.m
+++ +anjo/hia_sum_over_pol.m
@@ -1,4 +1,4 @@
-function [ionMat] = hia_sum_over_pol(ion4d)
+function [F_2d] = hia_sum_over_pol(F_3d)
 %ANJO.HIA_SUM_OVER_POL Sums a 4D ion HIA matrix over the polar angle.
 %
 %   ionMat = ANJO.HIA_SUM_OVER_POL(ion4d) Given ion4d of size Nx8x16x31,
@@ -7,31 +7,49 @@ function [ionMat] = hia_sum_over_pol(ion4d)
 %
 %   See also:   ANJO.GET_HIA_DATA
 %
+% 
+% if(length(size(ion4d)) == 4)
+%     tNum = size(ion4d,1);
+%     phiN = 16;
+%     d4 = 1;
+% else
+%     tNum = 1;
+%     phiN = size(ion4d,2);
+%     d4 = 0;
+% end
+% ionMat = zeros(tNum,phiN,31);
+% th = anjo.get_hia_values('theta')';
+% 
+% for i = 1:tNum
+%     for j = 1:8
+%         for k = 1:phiN
+%             for l = 1:31
+%                 if d4
+%                     ionMat(i,k,l) = ionMat(i,k,l)+ion4d(i,j,k,l)*sind(th(j));
+%                 else
+%                     ionMat(i,k,l) = ionMat(i,k,l)+ion4d(j,k,l)*sind(th(j));
+%                 end
+%             end
+%         end
+%     end
+% end
+% end
 
-if(length(size(ion4d)) == 4)
-    tNum = size(ion4d,1);
-    phiN = 16;
-    d4 = 1;
-else
-    tNum = 1;
-    phiN = size(ion4d,2);
-    d4 = 0;
-end
-ionMat = zeros(tNum,phiN,31);
-th = anjo.get_hia_values('theta')';
 
-for i = 1:tNum
-    for j = 1:8
-        for k = 1:phiN
-            for l = 1:31
-                if d4
-                    ionMat(i,k,l) = ionMat(i,k,l)+ion4d(i,j,k,l)*sind(th(j));
-                else
-                    ionMat(i,k,l) = ionMat(i,k,l)+ion4d(j,k,l)*sind(th(j));
-                end
-            end
+% New reasonable version:
+% Input is F_3d, size: 8x16Nx31
+
+
+phiN = size(F_3d,2);
+
+F_2d = zeros(phiN,31);
+th = anjo.get_hia_values('theta')'; %Polar angle
+
+for i = 1:8
+    for j = 1:phiN
+        for k = 1:31
+            fval = F_3d(i,j,k)*sind(th(i));
+            F_2d(j,k) = F_2d(j,k)+fval;
         end
     end
 end
-end
-
