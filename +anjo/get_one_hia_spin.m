89929abb5f8193d24ac3653d25d6b4c8ef4ab770
ajohlander <andreas.johlander@gmail.com>
2015-06-11 16:54:50 +0200

    changes structure, loss of performance

--- +anjo/get_one_hia_spin.m
+++ +anjo/get_one_hia_spin.m
@@ -1,4 +1,4 @@
-function [psdMat,tData] = get_one_hia_spin(t,scInd,mode)
+function [F,tData] = get_one_hia_spin(t,scInd,varargin)
 %ANJO.GET_ONE_HIA_SPIN get ion data for one full spin of CIS-HIA.
 %   psdMat = ANJO.GET_ONE_HIA_SPIN(t,scInd) returns ion data psdMat (matrix
 %   of size 8x16x31) for spacecraft scInd = 1,3. The spin returned has
@@ -10,17 +10,22 @@ function [psdMat,tData] = get_one_hia_spin(t,scInd,mode)
 %
 %   TODO: MAKE T START TIME.
 
+%pDa = {'3d'};
+pEn = {'full','half'};
 
-if(nargin == 2)
-    mode = 'default';
-end
+enMode = anjo.incheck(varargin,pEn);
 
 tint = [t-10,t+10];
-[psdArray,tArray] = anjo.get_hia_data(tint,scInd,mode);
+[~,tArray] = anjo.get_hia_data(tint,scInd,enMode);
+[F_3d,tvec] = anjo.get_hia_data(tint,scInd,enMode,'3d');
 
 dataInd = anjo.find_closest_index(t,tArray);
 tData = tArray(dataInd:dataInd+1);
+tInd = find(tvec==tData(1));
 
-psdMat = squeeze(psdArray(dataInd,:,:,:));
+% Chooses one spin
+F = F_3d(:,tInd:tInd+15,:);
+
+%F_3d = squeeze(F_4d(dataInd,:,:,:));
 
 end
