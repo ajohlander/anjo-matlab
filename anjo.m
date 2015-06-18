35e1dba97812c9cb72a3c9feaf2c4d290ace61d8
ajohlander <andreas.johlander@gmail.com>
2015-06-18 12:47:15 +0200

    adds option to display version

--- anjo.m
+++ anjo.m
@@ -2,18 +2,24 @@ function [varargout] = anjo(varargin)
 %ANJO General info
 %   
 %   ANJO('path') Returns path to repo.
-%   
+%
+%   ANJO('ver') Returns version of repo.
+%
 %   Author: Andreas Johlander
 
 if(nargin==0)
     help anjo
 else
-    pm = {'path'};
+    pm = {'path','ver'};
     md = anjo.incheck(varargin,pm);
     
     switch lower(md)
         case 'path'
             varargout{1} = fileparts(which('anjo.m'));
+        case 'ver'
+            fid = fopen('+anjo/Contents.m');
+            verstr = fgets(fid);
+            varargout{1} = verstr(3:end);
     end
 end
 end
