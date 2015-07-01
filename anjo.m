d83bdac25d2bebdffa2cd9b9b24cdf12f48fc2cd
ajohlander <andreas.johlander@gmail.com>
2015-07-01 10:53:41 +0200

    adds more options

--- anjo.m
+++ anjo.m
@@ -7,20 +7,27 @@ function [varargout] = anjo(varargin)
 %
 %   Author: Andreas Johlander
 
-if(nargin==0)
-    help anjo
-else
-    pm = {'path','ver'};
+
+    pm = {'check','help','path','ver'};
     md = anjo.incheck(varargin,pm);
     
     switch lower(md)
+        case 'check'
+            disp(['anjo-matlab ',anjo('ver')])
+            irf('check')
+        case 'help'
+            help anjo
         case 'path'
             varargout{1} = fileparts(which('anjo.m'));
         case 'ver'
             fid = fopen('+anjo/Contents.m');
             verstr = fgets(fid);
-            varargout{1} = verstr(3:end);
+            if(nargout == 1)
+                varargout{1} = verstr(3:end);
+            else
+                fprintf(verstr(3:end));
+            end
     end
-end
+
 end
 
