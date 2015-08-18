b475785ac0a2f8d2f271bd19c5cb29f499dd52f8
ajohlander <andreas.johlander@gmail.com>
2015-08-18 17:22:41 +0200

    find_closest_index now works with vectors

--- +anjo/find_closest_index.m
+++ +anjo/find_closest_index.m
@@ -1,19 +1,25 @@
 function [index] = find_closest_index(num,vec)
 %ANJO.FIND_CLOSEST_INDEX Finds relevant index of a vector
+%   
 %   index = ANJO.FIND_CLOSEST_INDEX(num, vec) returns the index of the vector
-%   vec where vec is the closest to the number num. vec must be increasing.
+%   vec where vec is the closest to the number num. vec must be increasing,
+%   num can be a vector or a scalar.
 
-index = find(num>=vec, 1, 'last' );
+nnum = length(num);
+index = zeros(1,nnum);
 
-if index == length(vec)
-    return;
-elseif(num-vec(index) >= vec(index+1)-num)
-    index = index+1;
+for i = 1:nnum
+    ind = find(num(i)>=vec, 1, 'last' );
+    if isempty(ind)
+        index(i) = 1;
+    elseif ind == length(vec)
+        index(i) = ind;
+    elseif(num(i)-vec(ind) >= vec(ind+1)-num(i))
+        index(i) = ind+1;
+    else
+        index(i) = ind;
+    end
+    
 end
-
-if isempty(index)
-    index = 1;
-end
-
 end
 
