634821749bc75d9a5165b7fef533fa96c2293400
ajohlander <andreas.johlander@gmail.com>
2015-08-24 15:34:51 +0200

    changes syntax

--- +anjo/get_hia_values.m
+++ +anjo/get_hia_values.m
@@ -1,36 +1,38 @@
-function [x1,x2,x3] = get_hia_values(value)
+function [x1,x2,x3] = get_hia_values(varargin)
 %ANJO.GET_HIA_VALUES Returns instrument parameters for HIA
-%   [x1,x2,x3] = ANJO.GET_HIA_VALUES(value) returns some parameters as given 
-%   by the CIS-HIA team.
+%   
+%   [x1,x2,x3] = ANJO.GET_HIA_VALUES(value,resolution) returns some
+%   parameters as given by the CIS-HIA team.
 %   
 %   value:
-%       'all'   - returns all parameters [th,phi,etab].
+%       'all'   - returns all parameters [th,phi,etab], default.
 %       'theta' - returns polar angle in degrees.
 %       'phi'   - returns azimuthal angle in degrees.
 %       'etab'  - returns energy values in eV.
-%       'half'  - returns energy with half resolution
 %
+%   resolution:
+%       'full'  - returns energy with half resolution, default.
+%       'half'  - returns energy with half resolution.
 
-if nargin == 0
-    x1 = get_theta();
-    x2 = get_phi();
-    x3 = get_e_tab();
-elseif(strcmp(value,'all'))
-    x1 = get_theta();
-    x2 = get_phi();
-    x3 = get_e_tab();
-elseif(strcmp(value,'theta'))
-    x1 = get_theta();
-elseif(strcmp(value,'phi'))
-    x1 = get_phi();
-elseif(strcmp(value,'etab'))
-    x1 = get_e_tab();
-elseif(strcmp(value,'half'))
-    % Hack so that it is not nececcary to define half energy twice.
-    e0 = get_e_tab();
-    f0 = zeros(1,1,31);
-    [~,x1] = anjo.hia_recalc_psd(f0,e0); 
+pvel = {'all','theta','phi','etab'};
+pres = {'full','half'};
+
+[value,res] = anjo.incheck(varargin,pvel,pres);
+
+switch value
+    case pvel{1}
+        x1 = get_theta();
+        x2 = get_phi();
+        x3 = get_e_tab(res);
+    case pvel{2}
+        x1 = get_theta();
+    case pvel{3}
+        x1 = get_phi();
+    case pvel{4}
+        x1 = get_e_tab(res);
 end
+
+
 
 end
 
@@ -53,9 +55,18 @@ function phi = get_phi()
 %     end
 end
 
-function eTab = get_e_tab()
-    eTab = [28898.33 21728.22 16337.12 12283.63 9235.88 6944.32 ...
+function eTab = get_e_tab(res)
+    e0 = [28898.33 21728.22 16337.12 12283.63 9235.88 6944.32 ...
         5221.33 3925.84 2951.78 2219.40 1668.73 1254.69 943.39 ...
         709.32 533.32 401.00 301.50 226.70 170.45 128.16 96.36 ...
         72.45 54.48 40.96 30.80 23.16 17.41 13.09 9.84 7.40 5.56];
+    if strcmp(res,'full')
+        eTab = e0;
+    elseif strcmp(res,'half')
+        % Hack so that it is not necessary to define half energy twice.
+        f0 = zeros(1,1,31);
+        [~,eTab] = anjo.hia_recalc_psd(f0,e0); 
+    else
+        error('Undefined energy resolution.')
+    end
 end
