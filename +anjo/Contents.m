ce840071eb50ebc7a2b540306bd38d8f9edaf174
ajohlander <andreas.johlander@gmail.com>
2015-08-28 16:19:33 +0200

    Updates Contents

--- +anjo/Contents.m
+++ +anjo/Contents.m
@@ -1,14 +1,16 @@
-% Version 0.10 18-Jun-2015
+% Version 0.20 28-Aug-2015
 %
 % Files
 %   afigure             - Quick way to call irf_plot
+%   ask                 - Ask a question.
 %   car2sph             - Transforms coordinates from cartesian to spherical
 %   cmap                - Custom colormap.
 %   compress_field      - Compresses 3D E- or B-field 
 %   e_jxb               - Calculates E = jxB/ne. 
 %   e_vxb               - Calculates E = jxB/ne. 
 %   fast_date           - Returns a date string
-%   find_closest_index  - Finds relevant index of a vector
+%   fci                 - Finds relevant index of a vector
+%   figmenu             - Helpful menu items.
 %   fix_x_label         - Tweaks axes to properly display x-label.
 %   get_3d_b_field      - Returns 3D FGM data in GSE.
 %   get_3d_e_field      - Returns 3D EFW data in ISR2.
@@ -20,6 +22,7 @@
 %   get_one_hia_spin    - Get ion data for one full spin of CIS-HIA.
 %   get_part_spin       - Get a partial spin
 %   gradient_line       - Plots a 2D line with a color gradient.
+%   hia_plot_velspace   - Plot CIS-HIA data in velocity space.
 %   hia_recalc_psd      - Recalculates PSD from 31 to 16 energy bins.
 %   hia_sum_over_pol    - Sums a 4D ion HIA matrix over the polar angle.
 %   hodo                - Plots B-field in hodograms in an LMN frame.
@@ -28,11 +31,12 @@
 %   incheck             - Matches inputs to parameters.
 %   intersect_line_data - Finds intersection between a line and data points
 %   label               - Adds label to axis.
+%   linearrow           - Draws arrows along a line
+%   linelength          - Calculate the length of a line.
 %   lorentz_1d          - Performs a 1D test patrticle simulation
 %   plot_3d_b_field     - Plot 3D FGM data in GSE.
 %   plot_3d_e_field     - Plot 3D EFW data in ISR2.
 %   plot_hia_subspin    - Plots data from HIA in phase space density [s^3km^-6].
-%   plot_ion_polar_isr2 - Plot CIS-HIA data in velocity space.
 %   plot_sim_vel        - Plots final velocity vs initial velocity
 %   print_fig           - Exports figure to file.
 %   progress_pie        - Pie chart showing progress of a calculation
@@ -40,3 +44,12 @@
 %   sc_azimuth          - Transforms plasma azimuthal angle to s/c azimuthal angle.
 %   sph2car             - Transforms coordinates from spherical to cartesian
 %   transform_e_field   - Transforms electric field to another inertial frame
+%   triangle            - Plots triangles.
+
+%   Marker              - Class for storing shapes.
+
+
+
+
+
+
