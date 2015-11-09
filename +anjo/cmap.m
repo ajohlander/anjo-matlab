51e0e5ebfdb93fb49f9f07097f4908814e85ab09
ajohlander <andreas.johlander@gmail.com>
2015-11-09 13:43:39 +0100

    allows axes input

--- +anjo/cmap.m
+++ +anjo/cmap.m
@@ -9,6 +9,8 @@ function [cmap] = cmap(varargin)
 %   c = ANJO.CMAP(color) Returns monochromatic colormap from white to
 %   black with specified color.
 %
+%   ANJO.CMAP(AX,...) Applies the given colormap to axes AX.
+%
 %   Colormap Names:
 %       'standard'  -   similar to irf_colormap
 %       'anjo'      -   orange and blue
@@ -33,10 +35,20 @@ function [cmap] = cmap(varargin)
 %   See also: IRF_COLORMAP
 
 %% Input
+ish = 0;
 if(nargin == 0)
     cMapMode = 'standard';
 else
-    cMapMode = varargin{1};
+    if ishandle(varargin{1})
+        cMapMode = 'standard';
+        ish = 1;
+        ax = varargin{1};
+        if nargin > 1
+            cMapMode = varargin{2};
+        end
+    else
+        cMapMode = varargin{1};
+    end
 end
 inmap = 1; %Is a colormap that is NOT read.
 
@@ -46,61 +58,64 @@ if(ismember(cMapMode,xmaps))
 end
 
 %% Set which colors
-if(inmap)
-    switch lower(cMapMode) % Special colormaps
-        case 'standard'
-            c = [255,255,255;...
-                043,255,255;...
-                000,255,000;...
-                255,255,000;...
-                255,255,000;...
-                255,000,000;...
-                000,000,255]/255;
-            
-        case 'anjo'
-            c = [1,1,1;...
-                1,0.7,0;...
-                0,0,0.8;...
-                0,0,0];
-        case 'speedway'
-            c = [255,255,255;...
-                255,255,000;...
-                255,000,000;...
-                000,000,200]/255;
-            
-        otherwise % Single color
-            
-            switch lower(cMapMode)
-                case 'red'
-                    cMiddle = [1,0,0];
-                case 'green'
-                    cMiddle = [0,1,0];
-                case 'blue'
-                    cMiddle = [0,0,1];
-                case 'yellow'
-                    cMiddle = [1,1,0];
-                case 'orange'
-                    cMiddle = [1,0.7,0];
-                case 'lime'
-                    cMiddle = [0.5,1,0];
-                case 'gray'
-                    cMiddle = [0.5,0.5,0.5];
-                case 'grey'
-                    cMiddle = [0.5,0.5,0.5];
-                otherwise
-                    if(inmap)
-                        error('Unknown color')
-                    end
-            end
-            if(inmap)
-                c = [1,1,1;cMiddle;0,0,0];
-            end   
-    end
+if ish
+    colormap(ax,anjo.cmap(cMapMode))
 else
-    % Read colormap from folder
-    cmstr = [anjo('path'),'/colmaps/',cMapMode,'.txt'];
-    c = dlmread(cmstr)/255;
-end
+    if(inmap)
+        switch lower(cMapMode) % Special colormaps
+            case 'standard'
+                c = [255,255,255;...
+                    043,255,255;...
+                    000,255,000;...
+                    255,255,000;...
+                    255,255,000;...
+                    255,000,000;...
+                    000,000,255]/255;
+                
+            case 'anjo'
+                c = [1,1,1;...
+                    1,0.7,0;...
+                    0,0,0.8;...
+                    0,0,0];
+            case 'speedway'
+                c = [255,255,255;...
+                    255,255,000;...
+                    255,000,000;...
+                    000,000,200]/255;
+                
+            otherwise % Single color
+                
+                switch lower(cMapMode)
+                    case 'red'
+                        cMiddle = [1,0,0];
+                    case 'green'
+                        cMiddle = [0,1,0];
+                    case 'blue'
+                        cMiddle = [0,0,1];
+                    case 'yellow'
+                        cMiddle = [1,1,0];
+                    case 'orange'
+                        cMiddle = [1,0.7,0];
+                    case 'lime'
+                        cMiddle = [0.5,1,0];
+                    case 'gray'
+                        cMiddle = [0.5,0.5,0.5];
+                    case 'grey'
+                        cMiddle = [0.5,0.5,0.5];
+                    otherwise
+                        if(inmap)
+                            error('Unknown color')
+                        end
+                end
+                if(inmap)
+                    c = [1,1,1;cMiddle;0,0,0];
+                end
+        end
+    else
+        % Read colormap from folder
+        cmstr = [anjo('path'),'/colmaps/',cMapMode,'.txt'];
+        c = dlmread(cmstr)/255;
+    end
 
 %% Create colormap
 cN = size(c,1);
@@ -109,6 +124,7 @@ cmap = zeros(64,3);
 
 for i = 1:64
     cmap(i,:) = interp1(x,c,i);
+end
 end
 
 end
