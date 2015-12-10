104711513281f15c5de15b10066b7681a57b305d
ajohlander <andreas.johlander@gmail.com>
2015-12-10 16:18:50 +0100

    changes syntax and now includes energy matrix for all versions of burst cdf

--- +anjo/+m/fpi_get_distr.m
+++ +anjo/+m/fpi_get_distr.m
@@ -1,19 +1,33 @@
-function [f1,f2,f3,f4] = fpi_get_distr(type,tint,id)
+function [f] = fpi_get_distr(type,tint,id)
 %ANJO.FPI_GET_DISTR Hacky way to get busrt distribution function.
 %   Will be deleted when there is a better alternative
 
+if length(id)>1
+    error('no')
+end
+
 switch lower(type(1))
-    case 'i'
-        
-        c_eval('f? = mms.db_get_ts(''mms?_fpi_brst_l1b_dis-dist'',''mms?_dis_brstSkyMap_dist'',tint);',id);
-        c_eval('phi? = mms.db_get_ts(''mms?_fpi_brst_l1b_dis-dist'',''mms?_dis_brstSkyMap_phi'',tint);',id);
+    case 'i' 
+        c_eval('f = mms.db_get_ts(''mms?_fpi_brst_l1b_dis-dist'',''mms?_dis_brstSkyMap_dist'',tint);',id);
+        c_eval('phi = mms.db_get_ts(''mms?_fpi_brst_l1b_dis-dist'',''mms?_dis_brstSkyMap_phi'',tint);',id);
+        c_eval('parity = mms.db_get_ts(''mms?_fpi_brst_l1b_dis-dist'',''mms?_dis_stepTable_parity'',tint);',id);
     case 'e'
         error('no')
 end
 
-c_eval('f?.userData.phi = phi?;',id)
+[e0,e1,~,~] = anjo.m.fpi_vals;
 
+nt = length(f.time);
+emat = zeros(nt,32);
+id0 = find(parity.data==0);
+id1 = find(parity.data==1);
 
+emat(id0,:) = repmat(e0,length(id0),1);
+emat(id1,:) = repmat(e1,length(id1),1);
+
+f.userData.phi = phi;
+f.userData.parity = parity;
+f.userData.emat = emat;
 
 end
 
