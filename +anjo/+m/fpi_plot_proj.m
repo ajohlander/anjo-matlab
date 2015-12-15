cfcd4fec2ad7f5529d57980a23b07abdfd70d2b9
ajohlander <andreas.johlander@gmail.com>
2015-12-15 14:41:25 +0100

    new, awesomer version

--- +anjo/+m/fpi_plot_proj.m
+++ +anjo/+m/fpi_plot_proj.m
@@ -1,199 +1,177 @@
-function out = fpi_plot_proj(varargin)
-%ANJO.M.FPI_PLOT_PROJ Plots ion data in velocity space.
+function varargout = fpi_plot_proj(varargin)
+%ANJO.M.FPI_PLOT_PROJ Plots projected ion data in velocity space.
 %
 %   ANJO.M.FPI_PLOT_PROJ(AX,...) Plots in axes AX, initiates a figure if
 %   omitted.
 %
-%   ANJO.M.FPI_PLOT_PROJ(F,t) Plots particle data in velocity space in XY
-%   plane for TSeries object (struct) of sky map data for time point t. If
-%   t is an interval data is averaged over interval.
+%   ANJO.M.FPI_PLOT_PROJ(F,t,x1,x2) Plots particle data in velocity space 
+%   for sky map data F for time point t. If t is an interval, data is
+%   averaged over interval. x1 and x2 are normal vector that defines the
+%   projection plane. x1 and x2 *must* be perpendicular.
 %
-%   ANJO.M.FPI_PLOT_PROJ(...,plane,first_parity) Specify which plane to
-%   plot. first_parity determines in which order the energy tables are
-%   used.
+%   F must have the structure returned by anjo.m.fpi_get_distr.
 %
-%   Planes: 'xy', 'yz', 'xz'.
+%   See also: ANJO.M.FPI_GET_DISTR.
 %
-%   TODO: Implement plane 'yz'. Use correct energy tables for time
-%   averaging.
+%   TODO: Time averaging looks wierd; probably should only be 32 energy
+%   bins. Electrons?
 
 
 %% Input
-
-% Possible planes
-pp = {'xy','yz','xz'};
-
 ish = ishandle(varargin{1});
 if ish
     AX = varargin{1};
 else
-    AX = anjo.afigure(1,[8,6]);
+    AX = anjo.afigure(1,[8,8]);
 end
 
-if nargin < 2+ish
-    error('Not enough input parameters')
-else
-    tint = varargin{2+ish};
-    f = varargin{1+ish};
-    if nargin >= 3+ish
-        plane = varargin{3+ish};
-        if nargin == 4+ish
-            first_parity = varargin{4+ish};
-        else
-            first_parity = 0;
-        end
-    else
-        plane = pp{1};
-        first_parity = 0;
+F = varargin{1+ish};
+tint = varargin{2+ish};
+x1 = varargin{3+ish};
+x2 = varargin{4+ish};
+
+% string input
+if ischar(x1)
+    yl1 = x1;
+    switch lower(x1)
+        case 'x'
+            x1 = [1,0,0];
+        case 'y'
+            x1 = [0,1,0];
+        case 'z'
+            x1 = [0,0,1];
     end
+else
+    yl1 = '1';
 end
+if ischar(x2)
+    yl2 = x2;
+    switch lower(x2)
+        case 'x'
+            x2 = [1,0,0];
+        case 'y'
+            x2 = [0,1,0];
+        case 'z'
+            x2 = [0,0,1];
+    end
+else
+    yl2 = '2';
+end
+            
+% normalize for good measure
+x1 = x1/norm(x1);
+x2 = x2/norm(x2);
 
-% Guess all values in one line!!!
-[e0,e1,phi,th] = anjo.m.fpi_vals;
-u = irf_units;
-
-
-nTh = length(th);
-nPhi = length(phi);
-nEn = length(e0);
 
 %% Logs
 % Give some output about whats happening
-if length(tint) == 2
-    irf.log('w','Averaging particle data over time interval given')
-    % Also average over energy tables.
-    etab = geomean([e0;e1]);
-elseif first_parity == 0
-    etab = e0;
+
+if cross(x1,x2) ~= 0
+    error('x1 and x2 must be perpendicular')
 else
-    etab = e1;
+    irf.log('w',['Plotting ion data as a function of '...
+        ,num2str(round(x1,1)), ' and ', num2str(round(x2,1)),'.'])
 end
-v = sqrt(2.*etab.*u.e./u.mp)./1e3;
-
-irf.log('w',['Plotting in the ',plane, '-plane'])
-
 
 
 %% Data handling
+% Set parameters
+F4d = double(F.data);
+emat = F.userData.emat;
+u = irf_units;
+vmat = sqrt(2.*emat.*u.e./u.mp)./1e3;
+phi = F.userData.phi;
+th = F.userData.th;
+
+nTh = size(th,2);
+nPhi = size(phi,2);
+nEn = size(emat,2);
+
+
+% Time and velocity grid. Use correct energy table for single time input,
+% use 64 energy bins for time interval input
 if length(tint) == 2
+    irf.log('w','Averages ion data over time, this usually turns out wierd.')
     %error('Time interval averaging is not yet implemented')
-    idt = anjo.fci(tint.epochUnix,f.time.epochUnix,'ext');
-    f3d = squeeze(mean(f.data(idt(1):idt(2),:,:,:)));
+    idt = anjo.fci(tint.epochUnix,F.time.epochUnix,'ext');
+    % number of time steps
+    nt = length(F.time(idt(1):idt(2)));
+    T = F.time(idt);
+    % Velocity bin values
+    vg = sort([vmat(idt(1),:),vmat(idt(1)+1,:)]);
 else
-    idt = anjo.fci(tint.epochUnix,f.time.epochUnix,'ext');
-    %idt = find(tint.epoch<f.time.epoch,1);
-    T = f.time(idt);
-    f3d = squeeze(f.data(idt,:,:,:));
+    idt = anjo.fci(tint.epochUnix,F.time.epochUnix,'ext');
+    T = F.time(idt);
+    irf.log('w',['Uses ion data from time: ',T.utc,'.'])
+    % number of time steps
+    nt = 1;
+    % Velocity bin values
+    vg = vmat(idt,:);
 end
 
-% Format of F3d is assumed to be [E,phi,th]
-switch plane
-    case 'xy'
-        % 2D matrix to be filled.
-        f2d = zeros(nPhi,nEn);
-        
-        % Rebins data
-        for i = 1:nTh
-            % ft is a 2D thcut-out of 3D distribution for one theta angle
-            % [phi,E]
-            ft = squeeze(f3d(:,:,i))';
-            % new velocity table in xy-plane
-            vt = v*cosd(th(i));
-            % new indicies for the data
-            idv = anjo.fci(vt,v,'ext');
-            
-            % loops through phi and energy
-            for j = 1:nPhi
-                for k = 1:nEn
-                    % adds the data to the f2d matrix. cos(th) is a geometric
-                    % factor.
-                    f2d(j,idv(k)) = f2d(j,idv(k))+ft(j,k)*cosd(th(i)); %CHECK INDICIES!!
-                end
-            end
+% Number of velocity bins
+nV = length(vg);
+
+% Azimuthal angle bin values
+phig = linspace(-pi,pi,nPhi);
+
+[phiMesh,rMesh] = meshgrid(phig,vg); % Creates the mesh
+[X,Y] = pol2cart(phiMesh-pi/nPhi,rMesh);    % Converts to cartesian
+
+% Awesome 4D matricies ,[t,E,phi,th]
+TH = repmat(th*pi/180,1,1,nEn,nPhi); % now [t,th,E,phi]
+TH = permute(TH,[1,3,4,2]);
+PHI = repmat(phi*pi/180,1,1,nEn,nTh); % now [t,phi,E,th]
+PHI = permute(PHI,[1,3,2,4]);
+VEL = repmat(vmat,1,1,nPhi,nTh); % now [t,E,phi,th], correct!
+
+F4d_comp = F4d.*cos(TH);
+
+[VX,VY,VZ] = sph2cart(PHI,TH,VEL);
+
+V1 = VX.*x1(1)+VY.*x1(2)+VZ.*x1(3);
+V2 = VX.*x2(1)+VY.*x2(2)+VZ.*x2(3);
+[AZI,R] = cart2pol(V1,V2);
+
+F3Mesh = zeros(nt,nEn,nPhi);
+for m = idt
+    F3d = squeeze(F4d_comp(m,:,:,:));
+    %F3d = ones(32,32,16);
+    r = squeeze(R(m,:,:,:));
+    azi = squeeze(AZI(m,:,:,:));
+    
+    idv = anjo.fci(r,vg,'nan');
+    idazi = anjo.fci(azi,phig,'nan');
+    
+    for n = 1:nV
+        for k = 1:nPhi
+            F3Mesh(m,n,k) = nanmean(F3d(idv==n & idazi==k));
         end
-        
-        % Add one row of zeros, no new data
-        f2dex = zeros(size(f2d)+[1,0]);
-        f2dex(1:end-1,:) = f2d;
-        
-        
-        nPhi = length(phi);
-        % Adds phiP(17) = phi(1). The term 11.25 degres is to rotate the circle.
-        phiP = interp1([phi,phi(1)],linspace(1,33,nPhi+1))-360/64;
-        phiRad = phiP*pi/180;
-        
-        [PHI,R] = meshgrid(phiRad,v); % Creates the mesh
-        [X,Y] = pol2cart(PHI,R);    % Converts to cartesian
-        
-    case 'xz'
-        
-        % 2D matrix to be filled.
-        f2d = zeros(nTh*2,nEn);
-        
-        % Rebins data
-        for j = 1:nPhi
-            % ft is a 2D thcut-out of 3D distribution for one phi angle [th,E]
-            ft = squeeze(f3d(:,j,:))';
-            % Normal vector of the plane
-            normvec = [0,1,0];
-            
-            
-            for i = 1:nTh
-                for k = 1:nEn
-                    % Get angle between velocity vector and normal vector
-                    [X,Y,Z] = sph2cart(phi(j)*pi/180,th(i)*pi/180,1); % vn in cartesian
-                    nu = acosd(dot(normvec,[X,Y,Z]));
-                    
-                    %velocity projected on the plane
-                    vt = v(k)*sind(nu);
-                    idv = anjo.fci(vt,v,'ext');
-                    % adds the data to the f2d matrix. cos(th) is a geometric
-                    % factor.
-                    if X(1)>=0
-                        f2d(i,idv) = f2d(i,idv)+ft(i,k)*cosd(th(i));
-                    else
-                        f2d(33-i,idv) = f2d(33-i,idv)+ft(i,k)*cosd(th(i));
-                    end
-                    
-                    
-                end
-            end
-        end
-        
-        % Add one row of zeros, no new data
-        f2dex = zeros(size(f2d)+[1,0]);
-        f2dex(1:end-1,:) = f2d;
-        
-        thex = [th,th+180];
-        thP = interp1([thex,thex(1)],linspace(1,33,33))-360/64;
-        thRad = thP*pi/180;
-        
-        [TH,R] = meshgrid(thRad,v); % Creates the mesh
-        [X,Y] = pol2cart(TH,R);    % Converts to cartesian
-        
-        
-    otherwise
-        error(['Plane ', plane, ' is not yet implemented'])
+    end
 end
+
+% Average over time, if only one time step only squeeze
+F2Mesh = squeeze(nanmean(F3Mesh,1));
+
 
 %% Plotting
-pcolor(AX,X,Y,log10(f2dex')*30);
+pcolor(AX,X,Y,log10(F2Mesh.*1e30));
 shading(AX,'flat')
 
 % Adds colorbar
 hcb = colorbar(AX);
-anjo.label(hcb,'$\log{F}$ [s$^3$cm$^{-6}$]') %[s$^3$km$^{-6}$]
+anjo.label(hcb,'$\log{F}$ [s$^3$km$^{-6}$]') %[s$^3$km$^{-6}$]
 
-% Reverses the direction of the x-axis. The Sun is to the left!
-AX.XDir = 'reverse';
-
-anjo.label(AX,'x',['$v_{',plane(1),'}$ [kms$^{-1}$]'])
-anjo.label(AX,'y',['$v_{',plane(2),'}$ [kms$^{-1}$]'])
+anjo.label(AX,'x',['$v_{',yl1,'}$ [kms$^{-1}$]'])
+anjo.label(AX,'y',['$v_{',yl2,'}$ [kms$^{-1}$]'])
 
 axis(AX,'equal')
 
-if nargout == 1
-    out = T;
+if nargout >= 1
+    varargout{1} = F2Mesh;
+end
+if nargout == 2
+    varargout{2} = T;
 end
 
 end
