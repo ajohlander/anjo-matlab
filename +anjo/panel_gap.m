b82a203fd6a867f4d2cc60872673f55d7b0d3540
ajohlander <andreas.johlander@gmail.com>
2016-01-08 18:08:06 +0100

    adds panel_gap

---
+++ +anjo/panel_gap.m
@@ -0,0 +1,48 @@
+function [] = panel_gap(AX,d)
+%ANJO.PANEL_GAP Creates gap between panels
+%   Detailed explanation goes here
+
+
+% SHOULD SORT AXES ON POSITION(2)
+if nargin == 0
+    f = gcf;
+    AX = findall(f.Children,'Type','Axes');
+    d = 0.05;
+elseif nargin == 1
+    if ishandle(AX(1))
+        d = 0.05;
+    else
+        d = AX;
+        f = gcf;
+        AX = findall(f.Children,'Type','Axes');
+    end
+end
+
+N = length(AX);
+h = AX(1).Position(4);
+u1 = AX(1).Position(2);
+D = d*h;
+
+H = u1+h-AX(end).Position(2);
+hp = (H-(N-1)*D)/N;
+
+up = zeros(1,N);
+
+up(1) = u1+h-hp;
+
+AX(1).Position(2) = up(1);
+AX(1).Position(4) = hp;
+
+
+for n = 2:N
+    ax = AX(n);
+    
+    up(n) = up(1)-(D+hp)*(n-1);
+    
+    ax.Position(2) = up(n);
+    ax.Position(4) = hp;
+    
+end
+
+end
+
